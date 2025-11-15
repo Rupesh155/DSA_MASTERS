adb503939c0d6f57c0a166a07bc0dae572ff0aec
Surya Shrivastava <suryashrivatava019@gmail.com>
2025-11-15 14:49:24 +0530

    Added Files

---
+++ Linked_list/iterativesearch2.cpp
@@ -0,0 +1,74 @@
+// ITERATIVE SEARCH ON A LINKED LIST BY SECOND WAY ... by Surya Shrivastava
+#include <iostream>
+using namespace std;
+class Node {
+    public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+class List {
+    Node* head;
+    Node* tail;
+    public:
+    List() {
+        head = NULL;
+        tail = NULL;
+    }
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+        if(head == NULL) {
+            head = newNode;
+            tail = newNode;
+        }else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+        if(head == NULL) {
+            head = tail = newNode;
+        }else {
+            tail -> next = newNode;
+            tail = newNode;
+        }
+    }
+    void printList() {
+        Node* temp = head;
+        while(temp != NULL) {
+            cout << temp -> data << "->";
+            temp = temp -> next;
+        }
+        cout << "NULL\n";
+    }
+    int search(int val) {
+        Node* temp = head;
+        int count = 0;
+        while(temp->next != NULL) {
+            if(temp->data == val) {
+                return count;
+            }
+            temp = temp -> next;
+            count ++;
+        }
+        return -1;
+    }
+};
+int main() {
+    List ll;
+
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+    ll.printList(); //  1 -> 2 -> 3 -> NULL
+    ll.push_back(4);
+    ll.push_back(5);
+    ll.push_back(6);
+    ll.printList(); // 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> NULL
+    cout << " is found at the position " << ll.search(5) << ". \n";
+    return 0;
+}
